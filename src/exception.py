ad0739811a56fd78821f832f80e985d780df304a
Samir-Zade <samirzadeofficial@gmail.com>
2024-04-22 21:21:17 +0530

    second changes

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 
 
 def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()#exc_info fetch the message from exception class
+    _, _, exc_tb = error_detail.exc_info() #exc_info fetch the message from exception class
 
     file_name = exc_tb.tb_frame.f_code.co_filename
 
@@ -18,11 +18,11 @@ class CustomException(Exception):
         """
         :param error_message: error message in string format
         """
-        super().__init__(error_message)#super called the message from parent class
+        super().__init__(error_message) #super called the message from parent class
 
         self.error_message = error_message_detail(
             error_message, error_detail=error_detail
         )
 
-    def __str__(self):#it print the object indirectly error message
+    def __str__(self): #it print the object indirectly error message
         return self.error_message
--- src/logger.py
+++ src/logger.py
@@ -7,7 +7,7 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE) #getcwd - get current path
 
-os.makedirs(logs_path, exist_ok=True)#create directory
+os.makedirs(logs_path, exist_ok=True) #create directory
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
